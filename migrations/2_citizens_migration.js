2421fbd8d99da0694b7a10039bc581cc432ffca5
Ibrahim <ibrahimabouzied10@gmail.com>
2022-02-15 14:51:03 -0700

    Create TribalCouncil.sol

--- migrations/2_citizens_migration.js
+++ migrations/2_citizens_migration.js
@@ -1,5 +1,8 @@
 var Citizens = artifacts.require("Citizens");
+var TribalCouncil = artifacts.require("TribalCouncil");
 
 module.exports = function(deployer) {
-  deployer.deploy(Citizens);
+  deployer.deploy(Citizens).then(() => {
+    return deployer.deploy(TribalCouncil, Citizens.address);
+  });
 };
