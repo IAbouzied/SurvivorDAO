5a9c6f43b2904b1cddd69d787735ee73f6cf771c
Ibrahim <ibrahimabouzied10@gmail.com>
2022-02-18 15:07:33 -0700

    Add tests for minting

--- test/waffle/Citizens.test.js
+++ test/waffle/Citizens.test.js
@@ -8,14 +8,49 @@ const Citizens = require("../../build/waffle-contracts/Citizens.json");
 use(solidity);
 
 describe('Citizens', () => {
-  const [wallet, walletTo] = new MockProvider().getWallets();
-  let citizens;
+  const [orchestrator, wallet1, wallet2] = new MockProvider().getWallets();
+  let citizens, citizensSigned1, citizensSigned2;
 
-  beforeEach(async () => {
-    citizens = await deployContract(wallet, Citizens, []);
-  });
+  const CITIZEN_NAME_1 = "Socrates";
+  const CITIZEN_NAME_2 = "Plato";
 
-  it('Is correctly named', async () => {
-    expect(await citizens.name()).to.equal("Citizens");
+  describe('Minting', () => {
+    beforeEach(async () => {
+      citizens = await deployContract(orchestrator, Citizens, []);
+      citizensSigned1 = citizens.connect(wallet1);
+      citizensSigned2 = citizens.connect(wallet2);
+    });
+
+    it('Successfully mint a citizen', async () => {
+      const transaction = await citizensSigned1.mintNFT(CITIZEN_NAME_1);
+
+      const citizen = await citizens.getCitizen(1);
+      expect(citizen.name).to.equal(CITIZEN_NAME_1);
+      expect(citizen.exiled).to.equal(false);
+      expect(citizen.roundsSurvived).to.equal(0);
+
+      expect(await citizens.balanceOf(wallet1.address)).to.equal(1);
+      expect(await citizens.ownerOf(1)).to.equal(wallet1.address);
+    });
+
+    it('Limits ownership to one Citizen', async () => {
+      await citizensSigned1.mintNFT(CITIZEN_NAME_1);
+
+      await expect(citizensSigned1.mintNFT(CITIZEN_NAME_2)).to.be.reverted;
+    });
+
+    it('Multiple people can mint', async () => {
+      await citizensSigned1.mintNFT(CITIZEN_NAME_1);
+      await citizensSigned2.mintNFT(CITIZEN_NAME_2);
+
+      expect(await citizens.ownerOf(1)).to.equal(wallet1.address);
+      expect(await citizens.ownerOf(2)).to.equal(wallet2.address);
+    });
+
+    it('Stop minting if the game has started', async () => {
+      await citizens.startGame();
+
+      await expect(citizensSigned1.mintNFT(CITIZEN_NAME_1)).to.be.reverted;
+    });
   });
 });
